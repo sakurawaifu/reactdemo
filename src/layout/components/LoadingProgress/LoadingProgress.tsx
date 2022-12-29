762784020dea9827d009d75fdc7d5c4da85ae4b1
sakurawaifu <2606843065@qq.com>
2022-12-29 15:39:30 +0800

    调整 LoadingProgress 动画

--- src/layout/components/LoadingProgress/LoadingProgress.tsx
+++ src/layout/components/LoadingProgress/LoadingProgress.tsx
@@ -3,16 +3,18 @@ import clsmix from '../../../utils/clsmix'
 import BasePropsType from '../../../constant/type/BasePropsType'
 import classname from '../../../utils/classname'
 
-
 export type LoadingProgressProps = BasePropsType & {
   complete: boolean
 }
 
 const LoadingProgress = (props: LoadingProgressProps) => {
-  const statusClass = props.complete ? styles.complete : ''
-
   return (
-    <div className={clsmix(classname([styles.loadingProgress, statusClass]), props.className)}></div>
+    <div
+      className={clsmix(classname({
+        [styles.loadingProgress]: true,
+        [styles.complete]: props.complete
+      }), props.className)}
+    ></div>
   )
 }
 
