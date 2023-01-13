61193915df7cdd855b40bfb11e5d246ef0177e86
sakurawaifu <2606843065@qq.com>
2023-01-13 18:02:09 +0800

    EmojiCell 组件增加 popover，显示码点。

--- src/components/EmojiList/components/EmojiCell/EmojiCell.tsx
+++ src/components/EmojiList/components/EmojiCell/EmojiCell.tsx
@@ -1,16 +1,22 @@
 import styles from './EmojiCell.module.scss'
 import classnames from '@/utils/classnames'
 import { CProps } from '@/types/CProps'
+import { Popover } from 'antd'
+import UnicodeUtils from '@/utils/UnicodeUtils'
 
 export type EmojiCellProps = CProps<{
   codePoint: number
 }>
 
 const EmojiCell = (props: EmojiCellProps) => {
+  const formatStr = UnicodeUtils.format(props.codePoint)
+
   return (
-    <div className={classnames(styles.emojiCell, props.className)}>
-      {String.fromCodePoint(props.codePoint)}
-    </div>
+    <Popover content={'U+' + formatStr} mouseLeaveDelay={0}>
+      <div className={classnames(styles.emojiCell, props.className)}>
+        {String.fromCodePoint(props.codePoint)}
+      </div>
+    </Popover>
   )
 }
 
